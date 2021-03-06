fc955cd928027b6a78ce12ca8529221e0d65ca4c
Germain <germain@grabyo.com>
2021-03-06 23:21:48 +0000

    Make hangul.app a PWA with offline capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import { generateSW } from "rollup-plugin-workbox";
+
+const plugins = [reactRefresh()];
+
+if (process.env.NODE_ENV === "production") {
+  plugins.push(
+    generateSW({
+      swDest: "dist/service-worker.js",
+      globDirectory: "dist",
+      globPatterns: ["**/*.{html,css,js,svg}"],
+      skipWaiting: true,
+    })
+  );
+  plugins.push({
+    name: "add-service worker",
+    transformIndexHtml() {
+      return [
+        {
+          tag: "script",
+          attrs: {
+            type: "text/javascript",
+          },
+          children: `if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+              navigator.serviceWorker.register('/service-worker.js');
+            });
+          }`,
+          injectTo: "body",
+        },
+      ];
+    },
+  });
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins,
+});
